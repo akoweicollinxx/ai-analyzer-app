6540ca325d3bff25e7b9c8a88330861ce6ba27d6
akoweicollinxx <110723714+akoweicollinxx@users.noreply.github.com>
2025-08-26 21:41:47 +0100

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient-">Resimind</p>
+                <p className="text-2xl font-bold text-gradient-">Analyze</p>
             </Link>
             <Link to="/upload" className="primary-button w-fit">
                 <p className="text-2xl font-semibold text-gradient-">Upload Resume</p>
